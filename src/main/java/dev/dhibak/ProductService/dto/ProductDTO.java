d017e0dd1b32cc292e5bd86ee8c044415585e49e
Dhibak VS <dhibakvs06@gmail.com>
2025-04-23 14:19:48 +0530

    added Lombok version on pom.xml and added Data annotation in DTOs

--- src/main/java/dev/dhibak/ProductService/dto/ProductDTO.java
+++ src/main/java/dev/dhibak/ProductService/dto/ProductDTO.java
@@ -1,10 +1,12 @@
 package dev.dhibak.ProductService.dto;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Data
 public class ProductDTO {
     private int id;
     private String title;
--- src/main/java/dev/dhibak/ProductService/dto/Rating.java
+++ src/main/java/dev/dhibak/ProductService/dto/Rating.java
@@ -1,10 +1,12 @@
 package dev.dhibak.ProductService.dto;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Data
 public class Rating {
     private double rate;
     private int count;
